2547a81bf331990c6f4388af1d52e8ee9d0596fb
Serg1oA <almeida.sergiomc@gmail.com>
2024-06-19 22:23:03 +0100

    Finished project, working HTML, CSS and JS.

--- Homepage-script.js
+++ Homepage-script.js
@@ -1,20 +1,55 @@
-// create 16x16 grid of divs
+// selectors + 
 const gameBox = document.querySelector("#gameBox");
+const squaresInput = document.querySelector("#squaresInput");
+const submitSquaresInput = document.querySelector("#submitSquaresInput");
+let isClicked = false;
 
+// when button is clicked => clean the board, use input to create squares
+submitSquaresInput.addEventListener("click", function() {
+    boardCleaner();
+
+    if (squaresInput.value > 0 && squaresInput.value < 100) {
+        squares = squaresInput.value;
+        squaresInput.value = "";
+
+    }
+    for (let i = 0; i < squares; i++) {
+        for (let j = 0; j < squares; j++) {
+            appendGameSquare();
+        }
+    }
+});
+
+// function to clean the board
+function boardCleaner() {
+    const squaresToRemove = document.querySelectorAll("#gameSquare");
+    squaresToRemove.forEach(square => {
+        square.remove();
+    });
+}
+
+/* function to create game squares (and give them a variable flexBasis
+   property, so that the game board is always a square of squares) */
 function appendGameSquare() {
     const gameSquare = document.createElement("div");
     gameSquare.id = "gameSquare";
+
+    gameSquare.addEventListener("mousedown", function() {
+        isClicked = true;
+        gameSquare.classList.add("highlight");
+        
+    });
+
+    gameSquare.addEventListener("mouseenter", function() {
+        if (isClicked === true) {
+            gameSquare.classList.add("highlight");
+        };
+    });
+
+    gameSquare.addEventListener("mouseup", function() {
+        isClicked = false;
+    });
+
+    gameSquare.style.flexBasis = `calc(100% / ${squares})`;
     gameBox.appendChild(gameSquare);
 }
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        appendGameSquare();
-    }
-}
-
-
-
-// hover effect (toggle a class with background color)
-
-
